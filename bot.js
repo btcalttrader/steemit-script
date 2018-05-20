a4e7183f3ea7ea111292ab501db60b2682d01d34
Carlososuna11 <37004057+Carlososuna11@users.noreply.github.com>
2018-05-20 18:58:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,14 @@ const steem = require("steem");
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "db4free.net",// the db used
-    user: "YOUR USER",
-    password: "YOUR PASS",
-    database: "YOUR DB NAME"
+    host: "focustarsgames.com",// the db used
+    user: "focustar_Carlos",
+    password: "29772315",
+    database: "focustar_Navi_bot"
 });
 
 con.connect();
-var bot = new Eris("THE BOT TOKEN", { // We create a new instance of our bot (usually named client)
+var bot = new Eris("NDI0MzU4NzM5NjU1NjU1NDQ0.DeJ9bw.ijDf4YMT3ZB7OlKH-5bMy-NUnYI", { // We create a new instance of our bot (usually named client)
     disableEveryone: true, // Makes it programmatically impossible for the bot to mention @everyone
     getAllUsers: true // It fetches all users, Good for small bots. (recommend)
   });
