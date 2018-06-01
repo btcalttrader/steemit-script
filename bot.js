f8c63539d9c0cf7f4dbecea165fd3f78aa3ab7a0
Carlososuna11 <37004057+Carlososuna11@users.noreply.github.com>
2018-06-01 16:35:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,6 @@
 const Eris = require ("eris");
 const steem = require ("steem");
-var mysql = require ('mysql');
 
-var con = mysql.createConnection ({
-    host: "db4free.net", // el host que uso vino de aquí
-   user: "nanoelperro",
-    password: "29772315",
-   database: "nanoelperro"
-});
-
-con.connect ();
 var bot = new Eris ("NDQ3OTcxNzY3Mzc0NDQ2NjE2.DePWeQ.WF6IZISXKbTHMKgIKtx0uoGq3pM", {// Creamos una nueva instancia de nuestro bot (generalmente llamado cliente)
     disableEveryone: true, // Hace que sea programáticamente imposible para el robot mencionar @everyone
     getAllUsers: true // Atrapa a todos los usuarios, bueno para bots pequeños. (recomendar)
